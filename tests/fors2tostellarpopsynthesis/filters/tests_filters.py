a8c27b278d9507f67be2df99d811cab6923fc1e7
sylvielsstfr <dagoret@lal.in2p3.fr>
2023-11-19 23:12:42 +0100

    update

--- tests/fors2tostellarpopsynthesis/filters/tests_filters.py
+++ tests/fors2tostellarpopsynthesis/filters/tests_filters.py
@@ -4,6 +4,7 @@
 # pylint: disable=missing-final-newline
 # pylint: disable=W0612
 
+# python -m unittest tests_filters.py
 
 import unittest
 
@@ -24,6 +25,9 @@ class FiltersTestCase(unittest.TestCase):
         survey_list = ps.filters_surveylist
 
         self.assertTrue(np.allclose(index_list,[0,1,2,3,4,5,6,7,8,9,10]),msg="fors2tostellarpopsynthesis.filters.FilterInfo error")
+        #self.assertEqual(survey_list,['galex','galex','sdss','sdss','sdss','sdss','vircam','vircam','vircam','vircam','vircam'],msg="fors2tostellarpopsynthesis.filters.FilterInfo error")
+        #self.assertEqual(name_list,['galex_FUV','galex_NUV','sdss_u0','sdss_g0','sdss_r0','sdss_i0','vista_vircam_Z','vista_vircam_Y','vista_vircam_J','vista_vircam_H','vista_vircam_Ks'],msg="fors2tostellarpopsynthesis.filters.FilterInfo error")
+
 
 if __name__ == "__main__":
     unittest.main()
