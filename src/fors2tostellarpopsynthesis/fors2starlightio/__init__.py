efc9f28b2e2be9ff492cc3569ae502a61d79cd1e
sylvielsstfr <dagoret@lal.in2p3.fr>
2023-11-20 11:54:13 +0100

    fors2starlightio.py

---
+++ src/fors2tostellarpopsynthesis/fors2starlightio/__init__.py
@@ -0,0 +1,4 @@
+from .fors2starlightio import (Fors2DataAcess, SLDataAcess,
+                               convertflambda_to_fnu, flux_norm, ordered_keys)
+
+__all__ = ["ordered_keys","convertflambda_to_fnu","flux_norm","Fors2DataAcess","SLDataAcess"]
